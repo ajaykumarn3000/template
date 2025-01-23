7b1ffc72d6a03cf96d8c5fc1f52d989fbea09c48
Ajaykumar Nadar <ajaykumarn3000@gmail.com>
2025-01-24 00:13:20 +0530

    chore: downgraded Tailwind CSS to support shadcn/ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
 import { AliasOptions, defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 
 const root = path.resolve(__dirname, 'src');
 
 // https://vite.dev/config/
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
+	plugins: [react()],
 	resolve: {
 		alias: {
 			'@': root,
